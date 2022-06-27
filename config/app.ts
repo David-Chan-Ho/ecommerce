a8ab6bd7d0c7bb9372eeb0d98b9a2fabad313890
dho2 <dho2@ncsu.edu>
2022-06-27 02:25:38 -0400

    set up swagger docs

--- config/app.ts
+++ config/app.ts
@@ -1,10 +1,15 @@
 import express from 'express'
+import expressJSDocSwagger from 'express-jsdoc-swagger'
 import startDB from './startDB'
+import swaggerOptions from './swagger'
 
 // Initialize MongoDB
 startDB()
 
 // Constructs new instance of Express
 const app = express()
+
+// Set up jsdoc swagger
+expressJSDocSwagger(app)(swaggerOptions);
 
 export default app

637272ce7b186a33bed65827c7052dc8caffc025
dho2 <dho2@ncsu.edu>
2022-06-28 02:03:23 -0400

    add storybook and styled-components

---
+++ client/src/components/Button/Button.stories.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Button } from ".";
+
+export default {
+    title: "Components/Button",
+    component: Button,
+    argTypes: {
+        backgroundColor: { control: "color" },
+    },
+    parameters: {
+        layout: "centered",
+    },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => (
+    <Button {...args}>Button</Button>
+);
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+    primary: true,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {};
+
+export const Large = Template.bind({});
+Large.args = {
+    size: "large",
+};
+
+export const Small = Template.bind({});
+Small.args = {
+    size: "small",
+};
---
+++ client/src/components/Button/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import { darken, lighten } from "polished";
+
+interface ButtonProps {
+    primary?: boolean;
+    backgroundColor?: string;
+    size?: "small" | "medium" | "large";
+    onClick?: () => void;
+}
+
+export const Button = styled.button<ButtonProps>`
+    font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-weight: 700;
+    border: 0;
+    border-radius: 0.3em;
+    cursor: pointer;
+    display: inline-block;
+    line-height: 1;
+    padding: ${({ size }) =>
+        size === "small"
+            ? "10px 16px"
+            : size === "medium"
+            ? "11px 20px"
+            : "12px 24px"};
+    font-size: ${({ size }) =>
+        size === "small" ? "12px" : size === "medium" ? "14px" : "16px"};
+    color: ${({ primary }) => (primary ? "white" : "#333")};
+    background-color: ${({ primary }) => (primary ? "#1ea7fd" : "white")};
+    box-shadow: ${({ primary }) =>
+        primary ? "" : "rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset"};
+
+    &:hover {
+        color: ${({ primary }) =>
+            primary ? lighten(1, "white") : lighten(1, "#333")};
+        background-color: ${({ primary }) =>
+            primary ? darken(0.1, "#1ea7fd") : darken(0.1, "white")};
+        box-shadow: ${({ primary }) =>
+            primary ? "" : `green 0px 0px 0px 1px inset`};
+    }
+`;
